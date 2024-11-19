f5ba75578120c7b1655489d1e40f461e592c8172
Klaus <edith@Klauss-MacBook-Air.local>
2024-11-19 09:35:21 +0300

    collect static

--- patientordersystem/patientordersystem/settings.py
+++ patientordersystem/patientordersystem/settings.py
@@ -176,7 +176,9 @@ LOGOUT_REDIRECT_URL = '/'  # Redirect to HomeView after logout
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
